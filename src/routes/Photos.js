3ba2cabfa89cf098b4e86a382aa622c13cba0dc7
jzho954 <jzho954@aucklanduni.ac.nz>
2024-04-15 17:34:33 +1200

    added functional contact me

--- src/routes/Photos.js
+++ src/routes/Photos.js
@@ -7,7 +7,14 @@ import Navbar from "../components/Navbar";
 import KoreaImage1 from '../assets/korea2.jpg'
 import KoreaImage2 from '../assets/korea4.jpg'
 import KoreaImage3 from '../assets/korea5.jpg'
-import bg from "../assets/mexico.jpg";
+import KoreaImage4 from '../assets/korea3.jpg'
+
+
+import KoreaImage5 from '../assets/food.jpg'
+import KoreaImage6 from '../assets/food2.jpg'
+import KoreaImage7 from '../assets/hanoi1.jpg'
+import KoreaImage8 from '../assets/korea6.jpg'
+import bg from "../assets/korea-bgg.jpg";
 import Footer from "../components/Footer";
 const Photos = () => {
     const { pathname } = useLocation();
@@ -58,20 +65,38 @@ const Photos = () => {
           >
             <div className="item">
               <img src={KoreaImage1} alt="Image" />
-              <h5>Web Development</h5>
+              <h5>Korean street</h5>
             </div>
             <div className="item">
               <img src={KoreaImage2} alt="Image" />
-              <h5>Brand Identity</h5>
+              <h5>Winter in Korea</h5>
             </div>
             <div className="item">
               <img src={KoreaImage3} alt="Image" />
-              <h5>Logo Design</h5>
+              <h5>Valorant event </h5>
             </div>
             <div className="item">
-              <img src={KoreaImage1} alt="Image" />
-              <h5>Web Development</h5>
+              <img src={KoreaImage4} alt="Image" />
+              <h5>Hongdae during winter</h5>
             </div>
+
+            <div className="item">
+              <img src={KoreaImage5} alt="Image" />
+              <h5>Shake Shack in Korea</h5>
+            </div>
+            <div className="item">
+              <img src={KoreaImage6} alt="Image" />
+              <h5>Spanish Restaurant</h5>
+            </div>
+            <div className="item">
+              <img src={KoreaImage7} alt="Image" />
+              <h5>Hanoi Train Tracks</h5>
+            </div>
+            <div className="item">
+              <img src={KoreaImage8} alt="Image" />
+              <h5>First day in Korea</h5>
+            </div>
+
            
           </Carousel>
           <img className="background-image-left" src={bg} alt="Image" />
